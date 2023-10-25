2244245a88b0eddfa7c23e4606ceef8a61487dfe
MBAS89 <mohammedbadran1@hotmail.com>
2023-10-25 12:29:51 +0300

    doing the footer of the app

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export const Footer = () => {
+
+  const currentYear = new Date().getFullYear
   return (
-    <div>Footer</div>
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>Copyright © {currentYear} - All right reserved</p>
+      </aside>
+    </footer>
   )
 }
